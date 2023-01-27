4ce53b95bb438ab1a57132dffa98d63103304c50
CLEPIL <f18121@g.ichinoseki.ac.jp>
2023-01-27 20:47:32 +0900

    zaiko

--- components/ValueSet.vue
+++ components/ValueSet.vue
@@ -1,10 +1,11 @@
 <template>
   <v-row class="ma-0">
-    <v-col cols="6" align-self="center" class="pa-0">
+    <v-col cols="1" />
+    <v-col cols="5" align-self="center" class="pa-0">
       {{ check }}:
     </v-col>
     <v-col cols="3" class="pa-0">
-      <v-text-field v-model.number="value" inputmode="numeric" class="right" />
+      <v-text-field v-model.number="value" inputmode="numeric" class="right" @change="value_change" />
     </v-col>
     <v-col cols="3" align-self="center" class="pl-1">
       円/kg
@@ -15,12 +16,17 @@
 <script>
 export default {
   props: {
-    // eslint-disable-next-line vue/require-default-prop
-    check: String
+    check: String(),
+    index: Number()
   },
   data () {
     return {
       value: null
+    }
+  },
+  methods: {
+    value_change () {
+      this.$emit('value', this.value, this.index)
     }
   }
 }
--- pages/form2.vue
+++ pages/form2.vue
@@ -1,58 +1,104 @@
 <template>
   <v-app>
     ID : {{ this.$store.state.todo.Udata.email }}
-    <v-form class="form">
-      <v-row class="isCheck">
-        選択中: {{ checkbox }}
-      </v-row>
-      <v-row>
-        <v-col
-          v-for="list in items"
-          :key="list"
-          class="checkbox"
-          :cols="11"
-        >
-          <v-checkbox
-            v-model="checkbox"
-            :label="list"
-            :value="list"
-          />
-        </v-col>
-      </v-row>
+    <v-container style="margin-top: 20px; width: 500px">
+      <v-stepper v-model="e1">
+        <v-stepper-header>
+          <v-stepper-step :complete="e1 > 1" step="1">
+            種類選択
+          </v-stepper-step>
+          <v-divider />
+          <v-stepper-step :complete="e1 > 2" step="2">
+            価格設定
+          </v-stepper-step>
+          <v-divider />
+          <v-stepper-step step="3">
+            確認
+          </v-stepper-step>
+        </v-stepper-header>
 
-      <v-row class="button" justify="center" align-content="center">
-        <v-btn @click="wtInt()">
-          更新
-        </v-btn>
-        <v-dialog v-model="dialog" title="更新完了">
-          <v-card>
-            <v-card-text class="update">
-              更新を完了しました
-            </v-card-text>
-          </v-card>
-        </v-dialog>
-      </v-row>
-    </v-form>
+        <v-stepper-items>
+          <v-stepper-content step="1" class="pa-4">
+            <v-card class="mb-12">
+              <v-list-item>
+                <v-row>
+                  <v-col
+                    v-for="list in items"
+                    :key="list"
+                    class="checkbox"
+                    :cols="11"
+                  >
+                    <v-checkbox
+                      v-model="checkbox"
+                      :label="list"
+                      :value="list"
+                      @change="changeCheckbox"
+                    />
+                  </v-col>
+                </v-row>
+              </v-list-item>
+            </v-card>
+            <v-btn color="primary" @click="e1 = 2">
+              次へ
+            </v-btn>
+          </v-stepper-content>
+
+          <v-stepper-content step="2" class="pa-3">
+            <v-card class="mb-12">
+              <v-list-item v-for="(check, i) in checkbox" :key="check" class="pa-0" :check="check">
+                <ValueSet :check="check" :index="i" @value="changedValue" />
+              </v-list-item>
+            </v-card>
+            <v-btn color="primary" @click="e1 = 3">
+              次へ
+            </v-btn>
+            <v-btn @click="e1 = 1">
+              キャンセル
+            </v-btn>
+          </v-stepper-content>
+
+          <v-stepper-content step="3">
+            <v-card class="mb-12">
+              <v-col v-for="inv in inventory" :key="inv.name">
+                {{ inv.name }}: {{ inv.value }}円/kg
+              </v-col>
+            </v-card>
+            <v-btn color="primary" @click="WrightZaiko()">
+              完了
+            </v-btn>
+            <v-dialog v-model="dialog" title="更新完了">
+              <v-card>
+                <v-card-text class="update">
+                  更新を完了しました
+                </v-card-text>
+              </v-card>
+            </v-dialog>
+            <v-btn @click="e1 = 2">
+              キャンセル
+            </v-btn>
+          </v-stepper-content>
+        </v-stepper-items>
+      </v-stepper>
+    </v-container>
   </v-app>
 </template>
-
 <script>
 import { getDatabase, set, ref, update, get, child } from 'firebase/database'
+import ValueSet from '../components/ValueSet.vue'
 export default {
+  components: {
+    ValueSet
+  },
   data () {
     return {
+      e1: 1,
       id: null,
-      lists: [
-        { selected: 0, name: '要素1' },
-        { selected: 0, name: '要素2' },
-        { selected: 0, name: '要素3' },
-        { selected: 0, name: '要素4' },
-        { selected: 0, name: '要素5' },
-        { selected: 0, name: '要素6' }
-      ],
       checkbox: [],
+      previous_checkbox: [],
       items: [],
-      dialog: false
+      inventory: [],
+      headers: [],
+      dialog: true
     }
   },
   mounted () {
@@ -75,14 +121,17 @@ export default {
       })
   },
   methods: {
-    showModal () {
-      (this.dialog = true)(this.checkbox = [])
-    },
     wtInt () {
       const db = getDatabase()
       // eslint-disable-next-line no-console
       update(ref(db, 'int/' + this.$store.state.todo.Udata.email), {
         check: this.checkbox
+      })
+    },
+    WrightZaiko () {
+      const db = getDatabase()
+      update(ref(db, 'int/' + 'testman'), {
+        zaiko: this.inventory
       })
     },
     rvage () {
@@ -91,27 +140,25 @@ export default {
       set(ref(db, 'vages/'), {
         choices: ['ニンジン', 'ネギ', '大根']
       })
+    },
+    changedValue (value, index) {
+      this.inventory.splice(index, 1, { name: this.inventory[index].name, value })
+      // eslint-disable-next-line no-console
+      console.log(this.inventory)
+    },
+    changeCheckbox () {
+      if (this.previous_checkbox.length < this.checkbox.length) {
+        // push
+        const pushKey = this.checkbox.find(e => !this.previous_checkbox.includes(e))
+        this.previous_checkbox.push(pushKey)
+        this.inventory.push({ name: pushKey, value: null })
+      } else {
+        // pop
+        const popIndex = this.previous_checkbox.findIndex(e => !this.checkbox.includes(e))
+        this.previous_checkbox.splice(popIndex, 1)
+        this.inventory.splice(popIndex, 1)
+      }
     }
   }
 }
 </script>
-
-<style>
-.isCheck {
-  padding-top: 2vh;
-  justify-content: center;
-}
-.checkbox {
-  padding-left: 50px;
-}
-.update {
-  height: 3vh;
-  text-align: center;
-}
-.form {
-  text-align: center;
-}
-.button {
-  text-align: center;
-}
-</style>
